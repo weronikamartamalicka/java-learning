7ffb1cb1b235ff74b76ce20c3a575b9282fd6607
weronika malicka <weronikamarta.malicka@gmail.com>
2022-05-17 16:38:33 +0200

    7.2 Added package stream.immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,14 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 
 public class StreamMain {
     public static void main(String[] args) {
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("Doll",(text) -> text + "DEF");
