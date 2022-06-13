4a1e17454b164c43ad54bb8fd6e6546f8c6581ec
weronika malicka <weronikamarta.malicka@gmail.com>
2022-06-13 13:09:57 +0200

    13.1 Added com.kodilla.good.patterns.challenges package and MovieStore class

---
+++ kodilla-good-paterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,42 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public final class MovieStore {
+    private final List<String> ironManTranslations = new ArrayList<>();
+    private final List<String> avengersTranslations = new ArrayList<>();
+    private final List<String> flashTranslations = new ArrayList<>();
+    private final Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+
+    public Map<String, List<String>> getMovies() {
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+    public void showMovieTitles() {
+        getMovies();
+
+        String movieTitles = booksTitlesWithTranslations.entrySet().stream()
+                .map(e -> e.getValue())
+                .flatMap(List::stream)
+                .collect(Collectors.joining("!"));
+
+        System.out.print(movieTitles);
+    }
+}
---
+++ kodilla-good-paterns/src/main/java/com/kodilla/main/GoodPatternsApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.main;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+public class GoodPatternsApplication {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        movieStore.showMovieTitles();
+    }
+}
