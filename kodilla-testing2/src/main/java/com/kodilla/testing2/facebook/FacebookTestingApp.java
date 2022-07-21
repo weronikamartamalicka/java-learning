ebe8779b4f14f7047a7223b70c2336ac3b7b5617
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-07-21 14:56:22 +0200

    31.2 Added logic to fill birthsday info in new account on Facebook

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,38 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.awt.*;
+
+public class FacebookTestingApp {
+    public final static String XPATH_COOKIES_SELECT = "//div[@class = \"_9xo5\"]/button[1]";
+    public final static String XPATH_NEW_ACCOUNT = "//div[@class = \"_6ltg\"]/a";
+    public final static String XPATH_BIRTHDAY_TEXTAREA = "//div[@class = \"large_form\"]/div[4]/div[2]/span/span/select[1]";
+    public final static String XPATH_BIRTHMONTH_TEXTAREA = "//div[@class = \"large_form\"]/div[4]/div[2]/span/span/select[2]";
+    public final static String XPATH_BIRTHYEAR_TEXTAREA = "//div[@class = \"large_form\"]/div[4]/div[2]/span/span/select[3]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.facebook.com/");
+
+        driver.findElement(By.xpath(XPATH_COOKIES_SELECT)).click();
+        driver.findElement(By.xpath(XPATH_NEW_ACCOUNT)).click();
+
+
+        WebElement selectComboBirthDay = driver.findElement(By.xpath(XPATH_BIRTHDAY_TEXTAREA));
+        Select selectBirthDay = new Select(selectComboBirthDay);
+        selectBirthDay.selectByValue("26");
+
+        WebElement selectComboBirthMonth = driver.findElement(By.xpath(XPATH_BIRTHMONTH_TEXTAREA));
+        Select selectBirthMonth = new Select(selectComboBirthMonth);
+        selectBirthMonth.selectByValue("8");
+
+        WebElement selectComboBirthYear = driver.findElement(By.xpath(XPATH_BIRTHYEAR_TEXTAREA));
+        Select selectBirthYear = new Select(selectComboBirthYear);
+        selectBirthYear.selectByValue("1992");
+    }
+}
